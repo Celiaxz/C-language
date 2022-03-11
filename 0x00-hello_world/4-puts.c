ecdb6a8ecf9da1763ed5f8a9157edfd183b49170
Celiaxz <eze.cynthiaonyejere@gmail.com>
2022-03-10 17:13:31 -0800

    Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * Prints string
- *  
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-   return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+       	return (0);
 }
