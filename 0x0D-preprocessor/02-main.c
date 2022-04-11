ff0c5869cd2cc695527626cdd52bd4b870534c3c
Celiaxz <eze.cynthiaonyejere@gmail.com>
2022-04-11 15:10:36 -0700

    updated

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+/**
+*main - prints the name of the file it was compiled from
+*Return: Always 0.
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
